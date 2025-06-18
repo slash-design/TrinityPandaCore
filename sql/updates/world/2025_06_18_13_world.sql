71ca570909e62dd43d4fa4f3e36cc46ceca53eef
slash-design <elite_slash@hotmail.com>
2025-06-18 22:26:05 +0200

    [DB/Locales]: Implement some DE Locales
    - trinity_string

---
+++ sql/updates/world/2025_06_18_13_world.sql
@@ -0,0 +1,4 @@
+-- trinity_string
+UPDATE `trinity_string` SET `content_loc3`='|cff3DAEFF[Shop]|cffFFD800 Dein Guthaben beträgt: |cffFF0000' WHERE `entry`=15005;
+UPDATE `trinity_string` SET `content_loc3`='|cff3DAEFF[Shop]|cffFFD800 Vielen Dank für deinen Einkauf. Dein neues Guthaben beträgt: |cffFF0000' WHERE `entry`=15006;
+UPDATE `trinity_string` SET `content_loc3`='|cff3DAEFF[Shop]|cffFFD800 Dein Guthaben ist zu niedrig.  |cffFF0000' WHERE `entry`=15007;
