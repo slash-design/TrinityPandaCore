40c0847c13e2142cbb62af1a1642841c82f7e4c6
slash-design <elite_slash@hotmail.com>
2025-06-18 22:25:06 +0200

    [DB/Locales]: Implement some DE Locales
    - locales_battle_pay_group

---
+++ sql/updates/world/2025_06_18_12_world.sql
@@ -0,0 +1,8 @@
+-- locales_battle_pay_group
+UPDATE `locales_battle_pay_group` SET `name_loc3`='Empfohlen' WHERE `id`=1;
+UPDATE `locales_battle_pay_group` SET `name_loc3`='Reittiere' WHERE `id`=2;
+UPDATE `locales_battle_pay_group` SET `name_loc3`='Taschen' WHERE `id`=3;
+UPDATE `locales_battle_pay_group` SET `name_loc3`='Haustiere' WHERE `id`=4;
+UPDATE `locales_battle_pay_group` SET `name_loc3`='Spielzeuge' WHERE `id`=8;
+UPDATE `locales_battle_pay_group` SET `name_loc3`='WoW-Marke' WHERE `id`=10;
+UPDATE `locales_battle_pay_group` SET `name_loc3`='Dienste' WHERE `id`=11;
